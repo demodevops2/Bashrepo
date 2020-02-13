60016e8e6cb02a5d81af83edca654b055ec59939
demodevops2 <31069933+demodevops2@users.noreply.github.com>
2020-02-13 16:38:15 +0530

    updated

--- Variables and Comments.sh
+++ Variables and Comments.sh
@@ -4,7 +4,7 @@ Comments: lines of code which are not executed by your script but are helpfull t
 
 Variables: are container which stores some data inside them.
            Two types of variables :
-                                  1. system varibles --> created and maintained by os system ---> pre-defind varibles --> define varibales in capital latters.
+                                  1. system varibles --> created and maintained by os system ---> pre-defind varibles --> define varibales in capital letters.
                                   2. userdefind varibles
 --> variable name should not start with a number. 
                                   
