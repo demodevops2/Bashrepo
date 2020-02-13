4cb5234ea4c11c6f4aa7399a86b6fe1eb80f6d83
demodevops2 <31069933+demodevops2@users.noreply.github.com>
2020-02-13 16:20:47 +0530

    created first bash file

---
+++ Variables and Comments.sh
@@ -0,0 +1,9 @@
+###########  Shell Scripting Tutorial  #######
+
+Comments: lines of code which are not executed by your script but are helpfull to know information about your scripts.
+
+Variables: are container which stores some data inside them.
+           Two types of variables :
+                                  1. system varibles --> created and maintained by os system ---> pre-defind varibles --> define varibales in capital latters.
+                                  2. userdefind varibles
+                                  
